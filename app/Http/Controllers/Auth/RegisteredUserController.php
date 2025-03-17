0421c2376ddaf4501217d3cdb90f1baa12f9f5c6
Brad Shelton <bcshelton72@gmail.com>
2025-03-17 15:46:06 -0500

    Added profile picture handling to registration submission

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -9,6 +9,7 @@ use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Storage;
 use Illuminate\Validation\Rules;
 use Inertia\Inertia;
 use Inertia\Response;
@@ -30,17 +31,26 @@ class RegisteredUserController extends Controller
      */
     public function store(Request $request): RedirectResponse
     {
+
         $request->validate([
             'name' => 'required|string|max:255',
             'email' => 'required|string|lowercase|email|max:255|unique:'.User::class,
+            'profile_picture' => ['file', 'nullable', 'max:300'],
             'password' => ['required', 'confirmed', Rules\Password::defaults()],
         ]);
 
-        $user = User::create([
+        $data = [
             'name' => $request->name,
             'email' => $request->email,
             'password' => Hash::make($request->password),
-        ]);
+            'profile_picture' => $request->profile_picture,
+        ];
+
+        if ($request->hasFile('profile_picture')) {
+            $data['profile_picture'] = Storage::disk('public')->put('profile_pictures', $request->profile_picture);
+        }
+
+        $user = User::create($data);
 
         event(new Registered($user));
 
