03274de327d67be68b786f21e8bbe47a1e057acf
Brad Shelton <bcshelton72@gmail.com>
2025-03-17 16:35:00 -0500

    Added profile picture & default avatar to header

--- resources/js/Layouts/AuthenticatedLayout.vue
+++ resources/js/Layouts/AuthenticatedLayout.vue
@@ -7,6 +7,7 @@ import NavLink from '@/Components/NavLink.vue';
 import ResponsiveNavLink from '@/Components/ResponsiveNavLink.vue';
 
 const showingNavigationDropdown = ref(false);
+const dicebear = import.meta.env.VITE_DICEBEAR_URL
 </script>
 
 <template>
@@ -51,20 +52,15 @@ const showingNavigationDropdown = ref(false);
                                                 type="button"
                                                 class="inline-flex items-center rounded-md border border-transparent bg-white px-3 py-2 text-sm font-medium leading-4 text-gray-500 transition duration-150 ease-in-out hover:text-gray-700 focus:outline-none"
                                             >
-                                                {{ $page.props.auth.user.name }}
 
-                                                <svg
-                                                    class="-me-0.5 ms-2 h-4 w-4"
-                                                    xmlns="http://www.w3.org/2000/svg"
-                                                    viewBox="0 0 20 20"
-                                                    fill="currentColor"
-                                                >
-                                                    <path
-                                                        fill-rule="evenodd"
-                                                        d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
-                                                        clip-rule="evenodd"
-                                                    />
-                                                </svg>
+                                                {{ $page.props.auth.user.name }}
+                                                <img
+                                                    class="object-cover w-8 h-8 rounded-full ml-4"
+                                                    :src="$page.props.auth.user.profile_picture
+                                                        ? 'storage/' + $page.props.auth.user.profile_picture
+                                                        : dicebear + '/initials/svg?seed=' + $page.props.auth.user.name"
+                                                    alt=""
+                                                />
                                             </button>
                                         </span>
                                     </template>
