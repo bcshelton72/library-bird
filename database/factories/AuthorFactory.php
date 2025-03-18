4e9b8043cc3979473e9c41d19cdee8f11cf40864
Brad Shelton <bcshelton72@gmail.com>
2025-03-18 14:37:10 -0500

    Added authors & seeder

--- database/factories/AuthorFactory.php
+++ database/factories/AuthorFactory.php
@@ -5,9 +5,9 @@ namespace Database\Factories;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 /**
- * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Category>
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Author>
  */
-class CategoryFactory extends Factory
+class AuthorFactory extends Factory
 {
     /**
      * Define the model's default state.
@@ -17,7 +17,8 @@ class CategoryFactory extends Factory
     public function definition(): array
     {
         return [
-            'name' => fake()->word(),
+            'first_name' => fake()->firstName(),
+            'last_name' => fake()->lastName(),
         ];
     }
 }
