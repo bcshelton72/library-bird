4e47d7899745f162b88a6d79d5354d54e5a91508
Brad Shelton <bcshelton72@gmail.com>
2025-03-18 16:16:49 -0500

    Refinement

--- database/migrations/2025_03_18_193844_create_books_table.php
+++ database/migrations/2025_03_18_193844_create_books_table.php
@@ -14,9 +14,9 @@ return new class extends Migration
         Schema::create('books', function (Blueprint $table) {
             $table->id();
             $table->string('title')->index();
-            $table->foreignId('author_id');
-            $table->foreignId('publisher_id');
-            $table->foreignId('category_id');
+            $table->foreignId('author_id')->references('id')->on('authors');
+            $table->foreignId('publisher_id')->references('id')->on('publishers');
+            $table->foreignId('category_id')->references('id')->on('categories');
             $table->date('checked_out_date')->nullable()->index();
             $table->decimal('average_rating', total: 3, places: 2)->default(0);
             $table->string('cover_image')->nullable();
