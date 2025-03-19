6ded1f283109a2c19050dd81ee90dee04b67df34
Brad Shelton <bcshelton72@gmail.com>
2025-03-18 22:09:17 -0500

    Added controllers

---
+++ app/Http/Controllers/WelcomeController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Foundation\Application;
+use Illuminate\Support\Facades\Route;
+use Inertia\Inertia;
+use Inertia\Response;
+
+class WelcomeController extends Controller
+{
+    /**
+     * Show the welcome page.
+     */
+    public function show(): Response
+    {
+        return Inertia::render('Welcome', [
+            'canLogin' => Route::has('login'),
+            'canRegister' => Route::has('register'),
+            'laravelVersion' => Application::VERSION,
+            'phpVersion' => PHP_VERSION,
+        ]);
+    }
+}
